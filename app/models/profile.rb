d545268b419c89ba51fb8ed3027d98ebfdb9a22b
Topstitch <kallen@spoonflower.com>
2015-03-02 19:31:12 -0500

    Sneaky little DateTime

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,18 +1,29 @@
 class Profile
   def initialize
-    profile_url = "https://api.github.com/users/Topstitch/repos"
-    @data = HTTParty.get(profile_url,
+    repo_url = "https://api.github.com/users/Topstitch/repos"
+    @repo_data = self.party_time(repo_url)
+
+    profile_url = "https://api.github.com/users/Topstitch"
+    @profile_data = self.party_time(profile_url)
+  end
+
+  def party_time(url)
+    HTTParty.get(url,
             :headers => {
               "Authorization" => "token #{ENV['GITHUB_TOKEN']}",
               "User-Agent" => "Topstitch"})
   end
 
   def profile_picture
-    @data[0]["owner"]["avatar_url"]
+    @profile_data["avatar_url"]
   end
 
   def username
-    @data[0]["owner"]["login"]
+    @profile_data["login"]
+  end
+
+  def joined_on
+    DateTime.parse(@profile_data["created_at"]).strftime('%b %d, %Y')
   end
 
 end
